846e50a78a488c7ce4c30b137a1352f95d0ddd1d
稲葉正健 <inabamasatake@inabaseikennoMacBook-Air.local>
2024-09-06 14:52:09 +0900

    modified QuizFetcher.spec.js

--- __tests__/src/data_fechers/QuizFetcher.spec.js
+++ __tests__/src/data_fechers/QuizFetcher.spec.js
@@ -7,8 +7,7 @@ describe('QuizFetcherのテスト',()=>{
         it('10件のクイズデータが手に入る',async()=>{
             const data=await QuizFetcher.fetch();
             console.log(data);
-            // expect(Array.isArray(data.results)).toStrictEqual(true);
-            // expect(data.results.length).toStrictEqual(10);
+            
         });
     });
 });
